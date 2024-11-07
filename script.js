d55d7677e4baf6ed16c4fa29b717dcd3545d6595
Lowky51 <allanfleaux3@gmail.com>
2024-11-07 15:26:49 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById('burger');
-        const navbarMenu = document.getElementById('navbar-menu');
-
-        burger.addEventListener('click', () => {
-            navbarMenu.classList.toggle('active');
-        });
